f711ff58c027a4183d40197c9f15c4200e2726f0
Reynald09 <reynalmanurung3@gmail.com>
2025-10-30 12:41:41 +0700

    Initial Commit

---
+++ Tugas SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tugas SwiftUI
+//
+//  Created by iCodeWave Community on 30/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tugas SwiftUI/Tugas_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tugas_SwiftUIApp.swift
+//  Tugas SwiftUI
+//
+//  Created by iCodeWave Community on 30/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Tugas_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
